a4868a825820cd4504d4892874564a06bb27a9a8
curvemade <Bobstripping@gmail.com>
2026-01-30 10:51:58 +0100

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,2 @@
-game.Players.LocalPlayer:Kick("Havoc is updating")
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/20c771b78c8a42b504b7d6feb4a669ed5fc274b5290c5b469929d6d09011098b/download"))()
+
